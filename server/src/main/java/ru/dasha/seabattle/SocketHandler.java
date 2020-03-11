e99e83e2ccdff7a4fb9a8afbffe965352556bae2
spinoglaz <spinoglaz@gmail.com>
2020-03-11 13:53:08 +0600

    Replace LibGDX with Phaser

---
+++ server/src/main/java/ru/dasha/seabattle/SocketHandler.java
@@ -0,0 +1,36 @@
+package ru.dasha.seabattle;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+
+        for(WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws IOException {
+        session.sendMessage(new TextMessage("Hello"));
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
---
+++ server/src/main/resources/static/seabattle.js
@@ -0,0 +1,36 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    scene: {
+        preload: preload,
+        create: create
+    },
+};
+
+var game = new Phaser.Game(config);
+
+var txtStyle = {
+    font: '18px Arial',
+    fill: '#00ffff',
+};
+
+function preload ()
+{
+    this.load.setBaseURL('http://labs.phaser.io');
+}
+
+function create ()
+{
+    text = this.add.text(10, 10, '', txtStyle);
+
+    protocol = location.protocol === "https:" ? "wss" : "ws";
+    ws = new WebSocket(protocol + '://' + location.host + '/ws');
+    ws.onmessage = function(event){
+        text.text += event.data;
+        text.text += "\n";
+    };
+    ws.onopen = function(event){
+        ws.send("hello from client");
+    };
+}
