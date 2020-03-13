1f7d4d570c5931b6721285eb218245bbecef5798
mofr <mofr@zond.org>
2020-03-13 11:52:03 +0700

    Group client logic to game object

--- server/src/main/resources/static/seabattle.js
+++ server/src/main/resources/static/seabattle.js
@@ -16,34 +16,44 @@ ui = {
     }
 };
 
-ui.startBattleButton.onclick = function() {
-    ui.mainMenu.classList.remove('active');
-    ui.showLoader("Waiting for a battle...");
-    ws.send(JSON.stringify({
-        startBattle: {},
-    }));
-};
-
-ui.placingShipsExitButton.onclick = function() {
-    ui.placingShips.classList.remove('active');
-    ui.mainMenu.classList.add('active');
-};
-
-ui.placingShipsField.oncontextmenu = function() {
-    return false;
-};
-
-ui.showLoader("Connecting to the server...");
-protocol = location.protocol === "https:" ? "wss" : "ws";
-ws = new WebSocket(protocol + '://' + location.host + '/ws');
-ws.onmessage = function(event){
-    var serverMessage = JSON.parse(event.data);
-    if (serverMessage.joinedToBattle) {
+game = {
+    start: function() {
+        ui.showLoader("Connecting to the server...");
+        protocol = location.protocol === "https:" ? "wss" : "ws";
+        ws = new WebSocket(protocol + '://' + location.host + '/ws');
+        ws.onmessage = function(event){
+            var serverMessage = JSON.parse(event.data);
+            if (serverMessage.joinedToBattle) {
+                game.onJoinedToBattle(serverMessage.joinedToBattle);
+            }
+        };
+        ws.onopen = function(event){
+            ui.hideLoader();
+            ui.mainMenu.classList.add('active');
+        };
+    },
+    startBattle: function() {
+        ui.mainMenu.classList.remove('active');
+        ui.showLoader("Waiting for a battle...");
+        ws.send(JSON.stringify({
+            startBattle: {},
+        }));
+    },
+    exitPlacingShips: function() {
+        ui.placingShips.classList.remove('active');
+        ui.mainMenu.classList.add('active');
+        // TODO leave the battle or reconnect websocket
+    },
+    onJoinedToBattle: function(battleInfo) {
         ui.hideLoader();
         ui.placingShips.classList.add('active');
     }
 };
-ws.onopen = function(event){
-    ui.hideLoader();
-    ui.mainMenu.classList.add('active');
+
+ui.startBattleButton.onclick = game.startBattle;
+ui.placingShipsExitButton.onclick = game.exitPlacingShips;
+ui.placingShipsField.oncontextmenu = function() {
+    return false;
 };
+
+game.start();
