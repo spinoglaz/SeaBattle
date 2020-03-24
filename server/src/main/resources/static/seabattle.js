d9d470a5e2f74f6f29471ba9be61ffb147e518fb
mofr <mofr@zond.org>
2020-03-24 21:37:18 +0700

    More optimal shuffle function

--- server/src/main/resources/static/seabattle.js
+++ server/src/main/resources/static/seabattle.js
@@ -3,7 +3,10 @@ function clamp(num, min, max) {
 }
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
 const copyToClipboard = str => {
