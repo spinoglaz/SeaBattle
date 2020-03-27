bbc312fe240d7ada1894b2846ca8593d834c114c
spinoglaz <spinoglaz@gmail.com>
2020-03-27 18:21:13 +0600

    Implement ShootingStrategy

--- bot/src/main/java/ru/dasha/seabattle/bot/ShootingStrategy.java
+++ bot/src/main/java/ru/dasha/seabattle/bot/ShootingStrategy.java
@@ -2,6 +2,15 @@ package ru.dasha.seabattle.bot;
 
 public class ShootingStrategy {
     public Shot shoot(ShootingField field) {
-        return null;
+        int x;
+        int y;
+        do {
+            x = (int) (Math.random() * field.getSize());
+            y = (int) (Math.random() * field.getSize());
+        } while (field.getCell(x, y) != ShootingField.Cell.UNCHARTED);
+        Shot shot = new Shot();
+        shot.x = x;
+        shot.y = y;
+        return shot;
     }
 }
