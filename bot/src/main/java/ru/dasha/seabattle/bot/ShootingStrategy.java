743e12fa93f91976a649b687cee530d0a16eec4d
spinoglaz <spinoglaz@gmail.com>
2020-03-31 18:11:13 +0600

    Implement hunt strategy

--- bot/src/main/java/ru/dasha/seabattle/bot/ShootingStrategy.java
+++ bot/src/main/java/ru/dasha/seabattle/bot/ShootingStrategy.java
@@ -4,13 +4,24 @@ public class ShootingStrategy {
     public Shot shoot(ShootingField field) {
         int x;
         int y;
+        for (x = 0; x < field.getSize(); ++x) {
+            for (y = 0; y < field.getSize(); ++y) {
+                if (field.getCell(x, y) == ShootingField.Cell.UNCHARTED && hasOccupiedNear(field, x, y)) {
+                    return new Shot(x, y);
+                }
+            }
+        }
         do {
             x = (int) (Math.random() * field.getSize());
             y = (int) (Math.random() * field.getSize());
         } while (field.getCell(x, y) != ShootingField.Cell.UNCHARTED);
-        Shot shot = new Shot();
-        shot.x = x;
-        shot.y = y;
-        return shot;
+        return new Shot(x, y);
+    }
+
+    private boolean hasOccupiedNear(ShootingField field, int x, int y) {
+        return (x > 0 && field.getCell(x - 1, y) == ShootingField.Cell.OCCUPIED) ||
+                (x < field.getSize() - 1 && field.getCell(x + 1, y) == ShootingField.Cell.OCCUPIED) ||
+                (y > 0 && field.getCell(x, y - 1) == ShootingField.Cell.OCCUPIED) ||
+                (y < field.getSize() - 1 && field.getCell(x, y + 1) == ShootingField.Cell.OCCUPIED);
     }
 }
--- bot/src/main/java/ru/dasha/seabattle/bot/Shot.java
+++ bot/src/main/java/ru/dasha/seabattle/bot/Shot.java
@@ -3,4 +3,9 @@ package ru.dasha.seabattle.bot;
 public class Shot {
     public int x;
     public int y;
+
+    public Shot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
